948c538e4143c527f0bd755bcf33f43ac37dd296
elpeRfecto <mmjkee@yandex.ru>
2022-04-11 19:14:07 +0300

    16.8 rectangle

---
+++ Rectangle.py
@@ -0,0 +1,19 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a * self.b
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+    def get_area_square(self):
+        return self.a ** 2
+
+class Cirle:
+    def __init__(self, a):
+        self.a
+    def get_area_circle(self):
+        return self.a ** 2 * 3.14
