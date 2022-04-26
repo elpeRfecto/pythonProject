9bb20e01bf60a48a25d1ff8257cca1a49360facf
elpeRfecto <mmjkee@yandex.ru>
2022-04-26 12:11:48 +0300

    18.6.2 telegram-бот (конвертер валют)

---
+++ telegram/config.py
@@ -0,0 +1,7 @@
+TOKEN = '5306111039:AAH4mTkE_0jCcCqu2rLCtUzqngk_XUOSo58'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ telegram/extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from telegram.config import keys
+
+
+class ConvertionException(Exception):
+    pass
+
+class MoneyExchange:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}\nУвидеть список всех доступных валют: /values"')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        patch = total_base * amount
+
+        return float(patch)
